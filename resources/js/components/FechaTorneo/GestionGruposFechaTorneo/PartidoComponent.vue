ff2a8c40f8b0e830c2dab90d69c2a8975758db29
Adolfo <adolfojam98@gmail.com>
2023-04-24 20:38:02 -0300

    feat: :sparkles: funciona el ir asignando jugadores a partidos de llaves

--- resources/js/components/FechaTorneo/GestionGruposFechaTorneo/PartidoComponent.vue
+++ resources/js/components/FechaTorneo/GestionGruposFechaTorneo/PartidoComponent.vue
@@ -4,12 +4,12 @@
     <v-card-text>
       <v-row>
         <v-col>
-          <p>{{ partido.jugador1.nombre }} {{ partido.jugador1.apellido }}
+          <p>{{ partido.jugador1?.nombre }} {{ partido.jugador1?.apellido }}
           </p>
           <v-text-field label="Sets" v-model="partido.setsJugador1" type="number"></v-text-field>
         </v-col>
         <v-col>
-          <p>{{ partido.jugador2.nombre }} {{ partido.jugador2.apellido }}
+          <p>{{ partido.jugador2?.nombre }} {{ partido.jugador2?.apellido }}
           </p>
           <v-text-field label="Sets" v-model="partido.setsJugador2" type="number"></v-text-field>
         </v-col>
--- resources/js/components/FechaTorneo/GestionGruposFechaTorneo/PartidosFaseLlavesCategoriaComponent.vue
+++ resources/js/components/FechaTorneo/GestionGruposFechaTorneo/PartidosFaseLlavesCategoriaComponent.vue
@@ -1,7 +1,72 @@
 <template>
     <div>
-        {{ partidos }}
-        <v-btn color="success" @click="generarPartidos(16)">text</v-btn>
+        <div class="d-flex">
+            <div v-for="fase in FASES_REVERSE">
+                <v-col v-if="faseTienePartidos(fase)">
+                    <v-row>
+                        <v-col>
+                            <h3>{{ fase }}</h3>
+                        </v-col>
+                    </v-row>
+                    <div v-for="partido in partidos" :key="partido.id">
+
+                        <!-- NODOS HOJAS -->
+                        <div v-if="esNodoHoja(partido)">
+                            <v-card outlined flat v-if="partido.fase == fase">
+                                <div>
+                                    <v-card-text>
+                                        <v-row>
+                                            <v-col>
+                                                <v-autocomplete :items="jugadoresDisponibles(partido)"
+                                                    :item-text="nombreCompletoJugador" return-object 
+                                                    v-model="partido.jugador1" ></v-autocomplete>
+
+                                                <v-text-field label="Sets" v-model="partido.setsJugador1" 
+                                                    type="number"></v-text-field>
+                                            </v-col>
+                                            <v-col>
+                                                <v-autocomplete :items="jugadoresDisponibles(partido)"
+                                                    :item-text="nombreCompletoJugador" return-object  
+                                                    v-model="partido.jugador2"></v-autocomplete>
+                                                <v-text-field label="Sets" v-model="partido.setsJugador2"
+                                                    type="number"></v-text-field>
+                                            </v-col>
+                                        </v-row>
+                                    </v-card-text>
+                                </div>
+                            </v-card>
+
+                        </div>
+
+                        <!-- NODOS COMUNES -->
+                        <div v-if="!esNodoHoja(partido)">
+                            <v-card outlined flat v-if="partido.fase == fase">
+                                <div>
+                                    <v-card-text>
+                                        <v-row>
+                                            <v-col>
+                                                <p>{{ partido.jugador1?.nombre }} {{ partido.jugador1?.apellido }}
+                                                </p>
+                                                <v-text-field label="Sets" v-model="partido.setsJugador1"
+                                                    type="number"></v-text-field>
+                                            </v-col>
+                                            <v-col>
+                                                <p>{{ partido.jugador2?.nombre }} {{ partido.jugador2?.apellido }}
+                                                </p>
+                                                <v-text-field label="Sets" v-model="partido.setsJugador2"
+                                                    type="number"></v-text-field>
+                                            </v-col>
+                                        </v-row>
+                                    </v-card-text>
+                                </div>
+                            </v-card>
+                        </div>
+                    </div>
+                </v-col>
+            </div>
+
+        </div>
+
     </div>
 </template>
 
@@ -18,18 +83,32 @@ export default {
             4: 'octavos',
             5: 'dieciseisavos',
             6: 'treintaidosavos'
-
         },
         partidos: []
     }),
     created() {
-        const partidosss = this.generarPartidos(32);
+        const partidosss = this.generarPartidos(8);
         for (const fase of Object.values(this.FASES)) {
-        this.partidos.push(...partidosss[fase]);
-    }
+            this.partidos.push(...partidosss[fase]);
+        }
 
     },
     methods: {
+        jugadoresDisponibles(partido) {
+            const jugadoresSeleccionados = this.partidos
+                .filter(p => p.jugador1 || p.jugador2)
+                .map(p => p.jugador1?.usuario_id || p.jugador2?.usuario_id);
+            return this.categoria.jugadoresAnotados.filter(jugador => (partido.jugador1?.usuario_id == jugador.usuario_id ||partido.jugador2?.usuario_id == jugador.usuario_id ) || !jugadoresSeleccionados.includes(jugador.usuario_id) );
+        },
+        nombreCompletoJugador(jugador) {
+            return `${jugador.nombre} ${jugador.apellido}`;
+        },
+        esNodoHoja(partido) {
+            return !this.partidos.some(p => p.idPartidoPadre == partido.id);
+        },
+        faseTienePartidos(fase) {
+            return this.partidos.some(partido => partido.fase == fase);
+        },
         generarPartidos(nroJugadores) {
             let partidosLlaves = [];
             let rondas = Math.ceil(Math.log2(nroJugadores));
@@ -95,6 +174,14 @@ export default {
         }
 
 
-    }
+    },
+    computed: {
+        FASES_REVERSE() {
+            return Object.values(this.FASES).reverse();
+        },
+
+      
+    },
+
 };
 </script>
