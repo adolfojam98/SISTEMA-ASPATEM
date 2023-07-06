c53ec38290520d914c93ebdbcf619c7e1514bd8a
Adolfo <adolfojam98@gmail.com>
2023-07-06 18:50:04 -0300

    feat: :sparkles: Se agrega anular cuota en el listado de cuotas

--- resources/js/components/Jugadores/UsuariosPagosComponent.vue
+++ resources/js/components/Jugadores/UsuariosPagosComponent.vue
@@ -2,32 +2,29 @@
   <div>
     <v-container class="pt-0 mx-0">
       <div class="d-flex mx-auto" style="justify-content: center;">
-          <h2>Administración de pagos</h2>
+        <h2>Administración de pagos</h2>
       </div>
 
       <div class="d-flex mt-4" style="align-items: center">
         <div style="width: 300px; ">
-          <v-autocomplete
-                v-model="usuarioSeleccionado"
-                :items="usuarios"
-                :item-text="nombreCompleto"
-                return-object
-                label="Ingrese el nombre del socio"
-          ></v-autocomplete>
+          <v-autocomplete v-model="usuarioSeleccionado" :items="usuarios" :item-text="nombreCompleto" return-object
+            label="Ingrese el nombre del socio"></v-autocomplete>
         </div>
-        
-          <v-btn class="mx-4" @click="buscarCuotasUsuario" color="primary">
-              Buscar
-          </v-btn>
 
-          <v-btn class="mx-4" @click="generarCuotasMasivas = true" color="primary">
-            Generar cuotas masivamente
-          </v-btn>
+        <v-btn class="mx-4" @click="buscarCuotasUsuario" color="primary">
+          Buscar
+        </v-btn>
 
+        <v-btn class="mx-4" @click="generarCuotasMasivas = true" color="primary">
+          Generar cuotas masivamente
+        </v-btn>
+        <div v-if="busco">
           <anular-cuotas @anularTodasLasCuotas="anularTodasLasCuotas"></anular-cuotas>
+        </div>
+
       </div>
-      
-      
+
+
       <!-- <v-row class="mt-2">
         <v-col cols="8">
           
@@ -54,89 +51,95 @@
 
     <v-container class="d-flex" style="justify-content: center;">
       <v-card>
-      <template>
-        <div style="width: 600px">
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-              <tr v-if="cuotasUsuario != ''">
-                <th class="text-left">Mes</th>
-                <th class="text-left">Año</th>
-                <th class="text-left">Monto</th>
-                <th class="text-left">Fecha Pago</th>
-                <th class="text-left"></th>
-              </tr>
-            </thead>
-            <tbody v-for="(cuota, index) in cuotasUsuario" :key="index">
-              <tr
-                style="cursor: pointer"
-                v-if="cuota?.pago?.fecha_pago != null"
-                @click="
-                  [(infoCuotaPaga = !infoCuotaPaga), (cuotaActual = cuota)]
-                "
-              >
-                <td>{{ monthNames && monthNames[new Date(cuota.periodo).getMonth()] }}</td>
-                <td>{{ new Date(cuota.periodo).getFullYear().toString() }}</td>
-                <td>${{ cuota.monto_total }}</td>
-                <td>{{ cuota?.pago?.fecha_pago && darFormatoFecha(cuota.pago.fecha_pago) }}</td>
-                <td>
-                  <div class="text-center">
-                    <v-chip color="success" dark> Pagado </v-chip>
-                  </div>
-                </td>
-              </tr>
+        <template>
+          <div style="width: 600px">
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                  <tr v-if="cuotasUsuario != ''">
+                    <th class="text-left">Mes</th>
+                    <th class="text-left">Año</th>
+                    <th class="text-left">Monto</th>
+                    <th class="text-left">Fecha Pago</th>
+                    <th class="text-left"></th>
+                  </tr>
+                </thead>
+                <tbody v-for="(cuota, index) in cuotasUsuario" :key="index">
+                  <tr style="cursor: pointer" v-if="cuota?.pago?.fecha_pago != null" @click="
+                    [(infoCuotaPaga = !infoCuotaPaga), (cuotaActual = cuota)]
+                    ">
+                    <td>{{ monthNames && monthNames[new Date(cuota.periodo).getMonth()] }}</td>
+                    <td>{{ new Date(cuota.periodo).getFullYear().toString() }}</td>
+                    <td>${{ cuota.monto_total }}</td>
+                    <td>{{ cuota?.pago?.fecha_pago && darFormatoFecha(cuota.pago.fecha_pago) }}</td>
+                    <td>
+                      <div class="text-center">
+                        <v-chip color="success" dark> Pagado </v-chip>
+                      </div>
+                    </td>
+                  </tr>
 
-              <tr
-                v-else
-                style="cursor: pointer"
-                @click="[(pagoCuota = !pagoCuota), , (cuotaActual = cuota)]"
-              >
-                <td>{{ monthNames && monthNames[new Date(cuota.periodo).getMonth()] }}</td>
-                <td>{{ new Date(cuota.periodo).getFullYear().toString() }}</td>
-                <td>${{ cuota.monto_total }}</td>
-                <td></td>
-               
-                <td>
-                  <div class="text-center">
-                    <v-chip color="error" dark> Pagar </v-chip>
-                  </div>
-                </td>
-                
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-        </div>
-      </template>
+                  <tr v-else style="cursor: pointer" @click="[(pagoCuota = !pagoCuota), , (cuotaActual = cuota)]">
+                    <td>{{ monthNames && monthNames[new Date(cuota.periodo).getMonth()] }}</td>
+                    <td>{{ new Date(cuota.periodo).getFullYear().toString() }}</td>
+                    <td>${{ cuota.monto_total }}</td>
+                    <td></td>
+
+                    <td>
+                      <div class="text-center">
+                        <v-chip class="mr-2" color="error" dark> Pagar </v-chip>
+                        <v-chip color="error" dark @click.stop="[modalAnularCuota = true, , (cuotaActual = cuota)]"> Anular </v-chip>
+                      </div>
+                    </td>
+
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </div>
+        </template>
       </v-card>
     </v-container>
 
     <v-dialog v-model="CrearCuotaModal" max-width="600px">
-      <crear-cuota
-        :usuarioID="usuarioSeleccionado.id"
-        @recargarCuotas="recargarCuotas = $event"
-      ></crear-cuota>
+      <crear-cuota :usuarioID="usuarioSeleccionado.id" @recargarCuotas="recargarCuotas = $event"></crear-cuota>
     </v-dialog>
 
     <v-dialog v-model="infoCuotaPaga" max-width="350px">
-      <info-cuota-paga
-        :usuario="usuarioSeleccionado"
-        :cuota="cuotaActual"
-      ></info-cuota-paga>
+      <info-cuota-paga :usuario="usuarioSeleccionado" :cuota="cuotaActual"></info-cuota-paga>
     </v-dialog>
 
     <v-dialog v-model="pagoCuota" max-width="700px">
-      <pago-cuota
-        :cuota="cuotaActual"
-        :usuario="usuarioSeleccionado"
-        @recargarCuotas="recargarCuotas = $event"
-      ></pago-cuota>
+      <pago-cuota :cuota="cuotaActual" :usuario="usuarioSeleccionado"
+        @recargarCuotas="recargarCuotas = $event"></pago-cuota>
     </v-dialog>
     <v-dialog v-model="generarCuotasMasivas" max-width="400px">
-      <generar-cuotas-masivas
-        @cerrarDialog="generarCuotasMasivas = !generarCuotasMasivas"
-      ></generar-cuotas-masivas>
+      <generar-cuotas-masivas @cerrarDialog="generarCuotasMasivas = !generarCuotasMasivas"></generar-cuotas-masivas>
     </v-dialog>
+
+    <v-dialog v-model="modalAnularCuota" max-width="600">
+            <v-card>
+            <template v-slot:activator="{ on, attrs }">
+
+            </template>
+            <h2 class="pa-3">
+                <center>Desea anular esta cuota?</center>
+            </h2>
+            <v-card-text>
+                <v-textarea name="input-7-1" label="Observación" v-model="observacion" rows="2"></v-textarea>
+
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="error" @click="modalAnularCuota = false">
+                    Cancelar
+                </v-btn>
+                <v-btn @click="[modalAnularCuota = false, anularCuota()]">
+                    ANULAR
+                </v-btn>
+            </v-card-actions>
+            </v-card>
+        </v-dialog>
   </div>
 </template>
 
@@ -156,6 +159,8 @@ export default {
       busco: false,
       recargarCuotas: false,
       generarCuotasMasivas: false,
+      modalAnularCuota: false,
+      observacion : "",
       monthNames: [
         "Enero",
         "Febrero",
@@ -176,6 +181,12 @@ export default {
     ...mapState("cuotas", ["tipoCuotasDetalles"]),
   },
   methods: {
+    anularCuota() {
+      console.log("anular");
+      axios.post(`/cuotas/${this.cuotaActual.id}/cancelar`).then((res) => {
+        this.callSnackbar(["Cuota Anulada correctamente", "success"]);
+      }).catch((error) => { this.callSnackbar(["No se pudo anular la cuota", "error"]) })
+    },
     ...mapActions(["callSnackbar"]),
     ...mapMutations("cuotas", ["setTipoCuotasDetalles"]),
 
@@ -252,13 +263,14 @@ export default {
     filtrarUsuariosNoSocios() {
       this.usuarios = this.usuarios.filter((usuario) => usuario.socio.socio);
     },
-    anularTodasLasCuotas(){
+    anularTodasLasCuotas() {
       this.cuotasUsuario.forEach(cuota => {
-         if(cuota?.pago?.fecha_pago == null){
-          axios.post(`/cuotas/${cuota.id}/cancelar`)
-      }
+        if (cuota?.pago?.fecha_pago == null) {
+          axios.post(`/cuotas/${cuota.id}/cancelar`).then(() => {
+            this.callSnackbar(["Cuotas anuladas correctamente", "success"]);
+          }).catch(e => this.callSnackbar(["Hubo un error en cancelar cuotas", "error"]));
+        }
       });
-     
     },
   },
 
@@ -275,7 +287,7 @@ export default {
 
     const responseUsuarios = await axios.get("/usuario");
     this.usuarios = responseUsuarios.data;
-    console.log('->usuarios: ',this.usuarios);
+    console.log('->usuarios: ', this.usuarios);
     this.filtrarUsuariosNoSocios();
 
     const resp = await axios.get("/tipo-detalles");
