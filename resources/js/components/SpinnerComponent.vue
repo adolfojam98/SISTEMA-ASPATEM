757c81fd028ddfce7781135b610f75bff4cb0923
Adolfo <adolfojam98@gmail.com>
2023-08-15 17:09:30 -0300

    revert: :fire: Se saca spinner por ahora

--- resources/js/components/SpinnerComponent.vue
+++ resources/js/components/SpinnerComponent.vue
@@ -24,39 +24,39 @@ export default {
   },
   methods: {
     enableInterceptor() {
-      this.axiosInterceptor = window.axios.interceptors.request.use(
-        (config) => {
-          if (!(config.url === this.URL_STATUS)) {
-            // Iniciar el timer al hacer la petición
-            this.requestTimer = setTimeout(() => {
-              this.isLoading = true;
-            }, 500);
-          }
+      // this.axiosInterceptor = window.axios.interceptors.request.use(
+      //   (config) => {
+      //     if (!(config.url === this.URL_STATUS)) {
+      //       // Iniciar el timer al hacer la petición
+      //       this.requestTimer = setTimeout(() => {
+      //         this.isLoading = true;
+      //       }, 500);
+      //     }
 
-          return config;
-        },
-        (error) => {
-          this.isLoading = false;
-          // Limpiar el timer en caso de error
-          clearTimeout(this.requestTimer);
-          return Promise.reject(error);
-        }
-      );
+      //     return config;
+      //   },
+      //   (error) => {
+      //     this.isLoading = false;
+      //     // Limpiar el timer en caso de error
+      //     clearTimeout(this.requestTimer);
+      //     return Promise.reject(error);
+      //   }
+      // );
 
-      window.axios.interceptors.response.use(
-        (response) => {
-          // Limpiar el timer al recibir la respuesta
-          clearTimeout(this.requestTimer);
-          this.isLoading = false;
-          return response;
-        },
-        (error) => {
-          // Limpiar el timer en caso de error
-          clearTimeout(this.requestTimer);
-          this.isLoading = false;
-          return Promise.reject(error);
-        }
-      );
+      // window.axios.interceptors.response.use(
+      //   (response) => {
+      //     // Limpiar el timer al recibir la respuesta
+      //     clearTimeout(this.requestTimer);
+      //     this.isLoading = false;
+      //     return response;
+      //   },
+      //   (error) => {
+      //     // Limpiar el timer en caso de error
+      //     clearTimeout(this.requestTimer);
+      //     this.isLoading = false;
+      //     return Promise.reject(error);
+      //   }
+      // );
     },
   },
   created() {
